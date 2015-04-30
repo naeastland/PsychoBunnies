50ad19b64ad39acbf033f2142da686207acbd751
naeastland <naeastland@hotmail.com>
2015-04-30 00:12:27 -0700

    Create main.c

---
+++ CptS121/Craps-Sim/main.c
@@ -0,0 +1,17 @@
+/*
+Programmer: Nate Eastland
+Class: Cpts 121, Fall 2014; Lab Section 7
+Programming Assignment: PA4
+Date: 10/7/14
+Description: This program simulates a version of the dice game of 'Craps'.
+*/
+
+
+#include "craps.h"
+
+int main()
+{	
+	srand((unsigned int) time (NULL));
+	display_options();
+	return 0;
+}
